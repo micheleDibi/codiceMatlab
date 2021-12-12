746ecfa19d79a0f311fdcc3f3c4fe3affb041d71
micheleDibi <94901756+micheleDibi@users.noreply.github.com>
2021-12-12 15:32:50 +0100

    Update fattorizzazioneLUconPivot.m

--- fattorizzazioneLUconPivot.m
+++ fattorizzazioneLUconPivot.m
@@ -9,8 +9,41 @@ for x = 1:n
 end
 
 for k = 1:m
+    
+    % controllo che il pivot sia nullo - eventualmente devo risolvere la
+    % situazione
+    while(A(k, k) == 0 && k < m) 
+        suppRiga = k;
+        % scorro tra le righe finché non trovo un pivot diverso da 0 e non
+        % supero il numero di righe definito
+        while(A(suppRiga, k) == 0 && suppRiga < m) 
+            suppRiga = suppRiga + 1;
+        end
+
+        %controllo che il valore trovato sia minore del numero di righe
+        %definito
+        if(suppRiga <= m)
+
+            % devo effettuare lo scambio delle due righe
+            for w = 1:n 
+                suppVal = A(k, w);
+                A(k, w) = A(suppRiga, w);
+                A(suppRiga, w) = suppVal;
+            end
+            
+        else
+            % introduzione della VARIANTE
+            % non ci sono elementi pivotali lungo la colonna, 
+            % dunque sono costretto a spostarmi di una colonna a destra
+            k = k + 1;
+        end
+        U = A;
+    end 
+
     for i = (k+1):m
+        
         molt = -(A(i, k) / A(k, k));
+
         for j = k:m
             U(i,j) = molt * A(k, j) + A(i, j);
         end
